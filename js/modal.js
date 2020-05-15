84d00ea9473a59fdf731b4f462f4ae4e4103d7dc
deniseleechewwoon <61216017+deniseleechewwoon@users.noreply.github.com>
2020-05-15 13:24:46 +0800

    create modal.js for modal function

---
+++ js/modal.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $("#myModal").on('show.bs.modal', function (e) {
+        var button = $(e.relatedTarget);
+        var sign = button.attr('data-sign');
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": 'https://aztro.sameerkumar.website?sign=' + sign + '&day=today',
+            "method": "POST",
+        }
+        //make horoscope call
+        $.ajax(settings)
+            .done(function (response) {
+
+                //do whatever I want here to update HTML
+                console.log(`Current Date:${response.current_date}`);
+                console.log(`Horoscope: ${response.description}`);
+
+                let currentDate = "Horoscope Reading for " + response.current_date;
+                $(".modal-title").html(currentDate);
+
+                let horoDescription = response.description;
+                $(".modal-body").html(horoDescription);
+
+            });
+    });
+});
